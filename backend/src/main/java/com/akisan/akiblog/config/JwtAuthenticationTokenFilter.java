3133c28e99a2decf2fad84293545edfa6e61961f
3Asterism <842384412@qq.com>
2022-12-23 14:50:38 +0800

    AkiBlog token报错返回给前端

--- backend/src/main/java/com/akisan/akiblog/config/JwtAuthenticationTokenFilter.java
+++ backend/src/main/java/com/akisan/akiblog/config/JwtAuthenticationTokenFilter.java
@@ -19,7 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 @Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
