412b81193e2713ba7d893992325b8eb25a033863
3Asterism <842384412@qq.com>
2022-12-23 17:20:06 +0800

    AkiBlog 修改Controller 增加易读性

--- backend/src/main/java/com/akisan/akiblog/controller/BlogCommentController.java
+++ backend/src/main/java/com/akisan/akiblog/controller/BlogCommentController.java
@@ -11,10 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,7 +31,7 @@ public class BlogCommentController {
     }
 
     @Secured({"ROLE_CODER","ROLE_ADMIN","ROLE_USER"})
-    @PostMapping("/deleteBlogComment")
+    @DeleteMapping("/deleteBlogComment")
     @ApiOperation(value = "删除博客评论")
     public resultForRequestConstant deleteComment(@RequestBody delCommentInfo delCommentInfo) {
         sysBlogCommentImpl.deleteComment(delCommentInfo);
