5c5980acd484c69b36579cdd454803f80a4e9314
3Asterism <842384412@qq.com>
2022-12-26 10:36:53 +0800

    AkiBlog 更新数据库SQL

--- sql/testdb_ver0.14.sql
+++ sql/testdb_ver0.14.sql
@@ -1,20 +1,20 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 10.30.2.60
+ Source Server         : 47.100.215.126
  Source Server Type    : MySQL
- Source Server Version : 80031
- Source Host           : localhost:3306
+ Source Server Version : 50740
+ Source Host           : 47.100.215.126:3306
  Source Schema         : testdb
 
  Target Server Type    : MySQL
- Target Server Version : 80031
+ Target Server Version : 50740
  File Encoding         : 65001
 
- Date: 08/12/2022 11:47:42
+ Date: 26/12/2022 10:35:15
 */
 
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
@@ -22,13 +22,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_blog`;
 CREATE TABLE `sys_blog`  (
-  `userid` int NOT NULL COMMENT '博客创建者id',
-  `blogcontent` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '博客内容',
+  `userid` int(11) NOT NULL COMMENT '博客创建者id',
+  `blogcontent` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '博客内容',
   `createtime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '博客创建时间',
-  `blogid` int NOT NULL AUTO_INCREMENT COMMENT '博客识别id 主键',
-  `blogtitle` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '博客标题',
+  `blogid` int(11) NOT NULL AUTO_INCREMENT COMMENT '博客识别id 主键',
+  `blogtitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '博客标题',
   PRIMARY KEY (`blogid`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 11 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = MyISAM AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_blog
@@ -39,11 +39,11 @@ CREATE TABLE `sys_blog`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_homepagemsg`;
 CREATE TABLE `sys_homepagemsg`  (
-  `msgcontent` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '消息内容',
-  `msgid` int NOT NULL AUTO_INCREMENT COMMENT '消息id 主键自增',
+  `msgcontent` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '消息内容',
+  `msgid` int(11) NOT NULL AUTO_INCREMENT COMMENT '消息id 主键自增',
   `msgcreatetime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '消息创建时间',
   PRIMARY KEY (`msgid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_homepagemsg
@@ -54,11 +54,11 @@ CREATE TABLE `sys_homepagemsg`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
 CREATE TABLE `sys_role`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '角色',
-  `description` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '描述',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '角色',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_role
@@ -73,11 +73,11 @@ INSERT INTO `sys_role` VALUES (4, 'ROLE_NORMAL', '游客');
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_permission`;
 CREATE TABLE `sys_role_permission`  (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `role_id` bigint NOT NULL COMMENT '角色id',
-  `permission_id` bigint NULL DEFAULT NULL COMMENT '权限id',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `role_id` bigint(20) NOT NULL COMMENT '角色id',
+  `permission_id` bigint(20) NULL DEFAULT NULL COMMENT '权限id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '角色权限表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色权限表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_role_permission
@@ -88,21 +88,21 @@ CREATE TABLE `sys_role_permission`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user`  (
-  `username` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL COMMENT '博客用户账号,非空',
-  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL COMMENT '博客用户密码,非空',
-  `nickname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL COMMENT '博客用户名称,非空',
-  `userid` int NOT NULL AUTO_INCREMENT COMMENT '博客用户id,非空',
-  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '博客用户邮箱',
-  `phonenum` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '博客用户手机号',
-  `address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '博客用户地址',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '博客用户账号,非空',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '博客用户密码,非空',
+  `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '博客用户名称,非空',
+  `userid` int(11) NOT NULL AUTO_INCREMENT COMMENT '博客用户id,非空',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '博客用户邮箱',
+  `phonenum` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '博客用户手机号',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '博客用户地址',
   `registertime` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `useravatar` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '博客用户头像',
-  `userrole` int NULL DEFAULT NULL COMMENT '博客用户权限/对应sys_role_id',
-  `useronline` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL COMMENT '用户是否在线,1在线,0离线',
+  `useravatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '博客用户头像',
+  `userrole` int(11) NULL DEFAULT NULL COMMENT '博客用户权限/对应sys_role_id',
+  `useronline` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '用户是否在线,1在线,0离线',
   `status` tinyint(1) NULL DEFAULT NULL COMMENT '用户状态，1-开启-0禁用',
   `passwordNonExpired` tinyint(1) NULL DEFAULT NULL COMMENT '密码是否失效，1-可用，0-失效',
   PRIMARY KEY (`userid`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 12 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = MyISAM AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_user
@@ -117,12 +117,12 @@ INSERT INTO `sys_user` VALUES ('abc123', '$2a$10$o7iUlfSXFJMfS3/AnEu7E.EYA8jI7xn
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user_role`;
 CREATE TABLE `sys_user_role`  (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `user_id` bigint NOT NULL COMMENT '用户id',
-  `role_id` bigint NULL DEFAULT NULL COMMENT '角色id',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `role_id` bigint(20) NULL DEFAULT NULL COMMENT '角色id',
   `enabled` tinyint(1) NULL DEFAULT 1 COMMENT '是否有效',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '用户角色关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户角色关联表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_user_role
@@ -137,15 +137,15 @@ INSERT INTO `sys_user_role` VALUES (4, 11, 3, 1);
 -- ----------------------------
 DROP TABLE IF EXISTS `usr_activity`;
 CREATE TABLE `usr_activity`  (
-  `getpictimes` int NULL DEFAULT NULL COMMENT '接口调用次数',
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id主键',
-  `description` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '接口名称',
+  `getpictimes` int(11) NULL DEFAULT NULL COMMENT '接口调用次数',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id主键',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '接口名称',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of usr_activity
 -- ----------------------------
-INSERT INTO `usr_activity` VALUES (5, 1, 'lolicon接口调用次数');
+INSERT INTO `usr_activity` VALUES (171, 1, 'lolicon接口调用次数');
 
 SET FOREIGN_KEY_CHECKS = 1;
